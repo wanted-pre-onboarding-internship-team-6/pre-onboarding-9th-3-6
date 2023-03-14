3037a3655879abc603cac42d741bf73fcd1728b0
bymine <frfr0723@gmail.com>
2023-03-14 20:00:30 +0900

    [#3] feat: 차트 구현
    fetch() 함수와 Object.keys()함수를 사용해 JSON 데이터를 불러와 시계열 차트 구현
    <Chart/> 컴포넌트와 ChartData의 datasets속성을 이용해 Area 형태의 그래프와 Bar 형태의 그래프가 존재하는 복합 그래프 구현
    ChartOptions의 scales 속성과 datasets의 yAxisID속성을 이용해 Area 그래프의 기준값을 value_area 값으로 설정, Bar 그래프의 기준값을 value_bar 값으로 설정 및 Y축 수치 표현

--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -1,3 +1,117 @@
+import { useEffect, useState } from 'react';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+  ChartOptions,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+);
+
+interface chartList {
+  type: string;
+  version: number;
+  response: {
+    [timestamp: string]: {
+      id: string;
+      value_area: number;
+      value_bar: number;
+    };
+  };
+}
+
 export default function ChartPage() {
-  return <div>ChartPage</div>;
+  const [charts, setCharts] = useState<chartList>({
+    type: '',
+    version: 0,
+    response: {},
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('./chart_list.json');
+      const data = await response.json();
+      setCharts(data);
+    }
+
+    fetchData();
+  }, []);
+
+  const data = {
+    labels: Object.keys(charts.response),
+    datasets: [
+      {
+        type: 'line' as const,
+        fill: true,
+        label: 'Area',
+        data: Object.keys(charts.response).map(
+          (e) => charts.response[e].value_area,
+        ),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132,0.5)',
+        yAxisID: 'areaY',
+      },
+      {
+        type: 'bar' as const,
+        label: 'Bar',
+        data: Object.keys(charts.response).map(
+          (e) => charts.response[e].value_bar,
+        ),
+        backgroundColor: 'rgb(75, 192, 192)',
+        yAxisID: 'barY',
+      },
+    ],
+  };
+
+  const options: ChartOptions = {
+    responsive: true,
+    scales: {
+      areaY: {
+        max: 200,
+        position: 'left',
+        title: {
+          display: true,
+          text: 'Area',
+        },
+      },
+      barY: {
+        position: 'right',
+        title: {
+          display: true,
+          text: 'Bar',
+        },
+      },
+    },
+    plugins: {
+      title: {
+        display: true,
+        text: `${charts.type} v${charts.version}`,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Chart type="bar" data={data} options={options} />
+    </div>
+  );
 }
