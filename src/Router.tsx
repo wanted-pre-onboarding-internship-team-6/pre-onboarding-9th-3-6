8fb9b962feca411d5d7e6b7f689da7d512defcf0
Sungrinhan <sungrin92@gmail.com>
2023-03-14 11:19:11 +0900

    feat: 모듈 설치, 라우터 생성
    1. emotion/styled, emotion/react 설치
    2. react-router-dom 생성
    3. tsconfig 에 jsxImportSource 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,3 +1,15 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import Home from './pages/home';
+
 export default function Router() {
-  return;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
