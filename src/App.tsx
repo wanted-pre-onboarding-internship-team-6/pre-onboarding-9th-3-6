eca3dfe954ef3cf9d2ad814c67ab4d0c8ff9b43e
jimmy neutron <95525638+dlwnstjrzz@users.noreply.github.com>
2023-03-16 01:18:25 +0900

    Feat/#13 junsuk (#3)
    * [#3] 시계열 차트 만들기 - chungu (#9)
    
    * [#3] refactor: linting
    
    * [#3] build: chart.js & react-chart-js-2 설치
    
    * [#3] feat: mock data 추가
    
    * [#3] feat: chart type 상수화
    
    * [#3] feat: ChartOptions 타입 정의
    
    
    react-chartjs-2의 Chart 컴포넌트에 들어가는 options 객체의 타입 오류를
    해결하기 위해 ChartOptions 타입을 import해 제너릭을 채워줬습니다.
    
    * [#3] feat: Chart 구현
    
    * [#3] refactor: mock data 위치 public으로 변경
    
    * [#3] refactor: 차트 데이터 import에서 fetch로 변경
    
    * [#3] refactor: eslint rule 추가
    
    * env: react-router-dom 및 styled-components 설치
    
    Browser Router 적용시키고 UI 를 위해 styled-components를 설치했습니다.
    
    관련 이슈: #13
    
    * feat: 툴팁 기능 및 필터 기능 구현
    
    ---------
    
    Co-authored-by: 이춘구 <60649092+Chun-gu@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,234 @@
-import { ClockChart } from './pages';
-export default function App() {
-  return <ClockChart />;
+
+import { useEffect, useState } from 'react';
+
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Filler,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
+import { useSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { CHART_TYPE } from '@/constants';
+
+import type { ChartOptions } from 'chart.js';
+
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  Filler,
+);
+
+function defineAxisRange(values: number[]) {
+  const min = Math.floor(Math.min(...values) * 0.5);
+  const max = Math.floor(Math.max(...values) * 1.5);
+
+  return { min, max };
 }
+
+interface ChartData {
+  [timestamp: string]: {
+    id: string;
+    value_bar: number;
+    value_area: number;
+  };
+}
+interface ButtonProps {
+  isClicked: boolean;
+}
+
+export default function App() {
+  const [chartData, setChartData] = useState<ChartData[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const labels = Object.keys(chartData);
+  const barData = Object.values(chartData).map((data) => data.value_bar);
+  const areaData: any = Object.values(chartData).map((data) => data.value_area);
+  const uniqueRegionData = [
+    ...new Set(Object.values(chartData).map((data) => data.id)),
+  ];
+  const regionData = Object.values(chartData).map((data) => data.id);
+  const selectedRegion: any = searchParams.get('region');
+  const barDataset = {
+    type: CHART_TYPE.bar,
+    label: 'bar_value',
+    data: barData,
+    yAxidID: 'bar',
+    borderWidth: 2,
+    borderColor: regionData.map((data) =>
+      data === selectedRegion ? '#00f83e' : 'rgba(255, 99, 132, 0.5)',
+    ),
+    backgroundColor: regionData.map((data) =>
+      data === selectedRegion ? '#00f83e' : 'rgba(255, 99, 132, 0.5)',
+    ),
+  };
+
+  const areaDataset = {
+    type: CHART_TYPE.line,
+    label: 'area_value',
+    data: areaData,
+    yAxisID: 'area',
+    borderWidth: 2,
+    borderColor: regionData.map((data) =>
+      data === selectedRegion ? '#00f83e' : 'rgba(53, 162, 235, 0.5)',
+    ),
+    backgroundColor: regionData.map((data) =>
+      data === selectedRegion ? '#00f83e' : 'rgba(53, 162, 235, 0.5)',
+    ),
+  };
+
+  const datasets = [areaDataset, barDataset];
+
+  const data = { labels, datasets };
+
+  const options: ChartOptions = {
+    onClick: handleChartClick,
+    interaction: {
+      mode: 'index',
+      intersect: false,
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          beforeTitle: (context) => {
+            const dataKey: any = context[0].label;
+
+            const id = chartData[dataKey]?.id;
+
+            return `지역: ${id}`;
+          },
+        },
+      },
+    },
+    scales: {
+      bar: {
+        type: 'linear',
+        display: true,
+        position: 'left',
+        title: {
+          display: true,
+          text: 'bar_value',
+        },
+      },
+      area: {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        title: {
+          display: true,
+          text: 'area_value',
+        },
+        ...defineAxisRange(areaData),
+      },
+    },
+  };
+  function handleChartClick(
+    _chart: any,
+    event: { index: any }[],
+    _elements: any,
+  ) {
+    if (!event) return;
+
+    const dataIndex = event[0].index;
+    const dataId: any = Object.values(chartData).filter(
+      (_, index) => index === dataIndex,
+    )[0].id;
+    setSearchParams({ region: dataId });
+  }
+  function handleButtonClick(e: { target: { innerHTML: any } }) {
+    setSearchParams({ region: e.target.innerHTML });
+  }
+  useEffect(() => {
+    async function fetchChartData() {
+      const response = await fetch('/flexsys_mock_data.json');
+      const data = await response.json();
+
+      Object.entries(data).map(([key, value]) => ({ [key]: value }));
+      setChartData(data.response);
+    }
+
+    if (chartData.length === 0) fetchChartData();
+  }, [chartData.length]);
+
+  return (
+    <>
+      <Filter>
+        <Description>지역을 선택해주세요: </Description>
+        <ButtonWrapper>
+          {uniqueRegionData.map((region) => {
+            return (
+              <Button
+                key={region}
+                onClick={handleButtonClick}
+                isClicked={region === selectedRegion}>
+                {region}
+              </Button>
+            );
+          })}
+        </ButtonWrapper>
+      </Filter>
+      {chartData.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+          <Chart type={CHART_TYPE.bar} data={data} options={options} />
+        </div>
+      )}
+    </>
+  );
+}
+
+const Button: any = styled.button<ButtonProps>`
+  background-color: ${(props) => (props.isClicked ? ' #142e15' : ' #4caf50')};
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 12px;
+  transition-duration: 0.4s;
+  &:hover {
+    background-color: #27702b;
+  }
+`;
+
+const Description = styled.span`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+`;
