b93b342b289820286879c05d52acf91df2e67736
Sungrinhan <sungrin92@gmail.com>
2023-03-16 10:36:04 +0900

    feat: hover기능, 필터링 기능 작성중...

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -34,6 +36,14 @@ ChartJS.register(
   BarController,
 );
 
+// interface dataType {
+//   key: number;
+//   id: '강남구' | '중랑구' | ' 노원구' | '성북구';
+//   value_area: number;
+//   value_bar: number;
+//   time: string;
+// }
+
 const Home = () => {
   const response = mockdata.response;
   const arrayOfData = Object.entries(response).map(([key, value], i) => ({
@@ -44,18 +54,49 @@ const Home = () => {
     time: key,
   }));
 
+  const regionData = [...new Set(Object.values(response).map((el) => el.id))];
+
   const TimeArray = Object.keys(response);
   const AreaArray = arrayOfData.map((el) => el.value_area);
   const BarArray = arrayOfData.map((el) => el.value_bar);
 
   const labels = TimeArray;
 
+  const selectList = ['전체', '강남구', '노원구', '성북구', '중랑구'];
+  const [selected, setSelected] = useState('전체');
+  /**
+   * tooltip 에 id 값을 출력하는 함수
+   */
   const afterTitle = (tooltipItems: TooltipItem<keyof ChartTypeRegistry>[]) => {
     const index = tooltipItems[0].dataIndex;
     const id = arrayOfData[index].id;
 
     return `ID: ${id}`;
   };
+
+  /**
+   * selected 에 따라 하이라이트 해주는 함수
+   */
+  // function colorize(elements: 'bar' | 'line') {
+  //   if (elements === 'bar') {
+  //     return (ctx: any) => {
+  //       console.log('bar ctx: ', ctx);
+  //       const xIndex = ctx.parsed.x;
+  //       const c = filteredData().includes(xIndex) ? '#F46300' : '#44DE28';
+
+  //       return c;
+  //     };
+  //   }
+  //   if (elements === 'line') {
+  //     return (ctx: any) => {
+  //       console.log('line ctx: ', ctx);
+  //       const xIndex = ctx.parsed.x;
+  //       const c = filteredData().includes(xIndex) ? '#320cf0' : '#7177d8';
+
+  //       return c;
+  //     };
+  //   }
+  // }
 
   const options: ChartOptions = {
     responsive: true,
@@ -93,6 +134,20 @@ const Home = () => {
       },
     },
 
+    // elements: {
+    //   bar: {
+    //     backgroundColor: colorize('bar'),
+    //     borderColor: colorize('bar'),
+    //     borderWidth: 2,
+    //   },
+    //   line: {
+    //     backgroundColor: colorize('line'),
+    //     borderColor: colorize('line'),
+    //     borderWidth: 2,
+    //     fill: true,
+    //   },
+    // },
+
     scales: {
       AreaY: {
         type: 'linear',
@@ -120,7 +175,7 @@ const Home = () => {
          * This is useful when there are multiple axes and you need to control which grid lines are drawn.
          */
         grid: {
-          drawOnChartArea: false,
+          drawOnChartArea: true,
         },
       },
     },
@@ -130,17 +185,27 @@ const Home = () => {
     labels,
     datasets: [
       {
-        type: 'line' as const,
+        type: 'line',
         label: 'Area',
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.8)',
+        borderColor: arrayOfData.map((data) => {
+          console.log(data);
+          console.log(data.id === selected);
+
+          return data.id === selected ? '#00f83e' : 'rgba(255, 99, 132, 0.5)';
+        }),
+        backgroundColor: arrayOfData.map((data) =>
+          data.id === selected ? '#00f83e' : 'rgba(255, 99, 132, 0.5)',
+        ),
+        // borderColor: 'rgb(255, 99, 132)',
+        // backgroundColor: 'rgba(255, 99, 132, 0.8)',
         borderWidth: 2,
         fill: true,
         data: AreaArray,
         yAxisID: 'AreaY',
+        pointRadius: 0,
       },
       {
-        type: 'bar' as const,
+        type: 'bar',
         label: 'Bar',
         borderColor: 'rgb(63, 49, 196)',
         backgroundColor: 'rgba(63, 49, 196, 0.1)',
@@ -151,9 +216,32 @@ const Home = () => {
     ],
   };
 
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const place = e.target.value;
+    setSelected(place);
+  };
+
+  /**
+   * filter 후 index 로만 이루어진 배열
+   */
+  // const filteredData = () => {
+  //   const filteredDataIndex = arrayOfData
+  //     .filter((data) => data.id === selected)
+  //     .map((el) => el.key);
+
+  //   return filteredDataIndex;
+  // };
+
   return (
     <div>
       <Chart type="bar" data={data} options={options} />
+      <select onChange={handleSelect} value={selected}>
+        {selectList.map((place) => (
+          <option value={place} key={place}>
+            {place}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
