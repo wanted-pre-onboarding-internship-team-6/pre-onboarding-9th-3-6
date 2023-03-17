64fb0135ab96d97a14b7fe619b23701ca613cdcb
Chun-gu <chungu.yi92@gmail.com>
2023-03-17 21:28:13 +0900

    [#30] style: 툴팁 리디자인

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -7,8 +7,11 @@ export const COLOR = {
 } as const;
 
 export const COLOR_CODE = {
+  lightGrey: '#eeeeee',
   hightlightRed: 'rgb(255, 52, 96, 1)',
   red: 'rgba(255, 52, 96, 0.4)',
   hightlightBlue: 'rgb(53, 162, 235, 1)',
   blue: 'rgba(53, 162, 235, 0.4)',
+  ashedBlue: 'rgba(107, 114, 142,0.9)',
+  darkBlue: 'rgb(205, 222, 255)',
 } as const;
