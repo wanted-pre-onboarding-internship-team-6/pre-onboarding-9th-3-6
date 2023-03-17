08f126129dda5be2bf3facc17f2389a7c5125b4f
Chun-gu <chungu.yi92@gmail.com>
2023-03-17 21:30:27 +0900

    [#30] refactor: 필터에 전체 선택 기능 추가
    Co-authored-by: theo-jin <theo-jin@users.noreply.github.com>
    Co-authored-by: dong-geon-Lee <dong-geon-Lee@users.noreply.github.com>
    Co-authored-by: jimmy neutron <dlwnstjrzz@users.noreply.github.com>
    Co-authored-by: Park Su Been <bymine@users.noreply.github.com>
    Co-authored-by: ggongjukim <ggongjukim@users.noreply.github.com>

--- src/components/RegionFilter.tsx
+++ src/components/RegionFilter.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 import { useSearchParams } from 'react-router-dom';
 
+import { COLOR_CODE } from '@/constants';
+
 interface Props {
   regions: string[];
 }
@@ -10,19 +12,35 @@ export default function RegionFilter({ regions }: Props) {
 
   return (
     <FilterContainer>
-      필터:
-      {regions.map((region) => (
-        <button key={region} onClick={() => setSearchParams({ region })}>
-          {region}
-        </button>
-      ))}
+      <Span>지역 선택:</Span>
+      <div>
+        <button onClick={() => setSearchParams({})}>전체</button>
+        {regions.map((region) => (
+          <button key={region} onClick={() => setSearchParams({ region })}>
+            {region}
+          </button>
+        ))}
+      </div>
     </FilterContainer>
   );
 }
 
 const FilterContainer = styled.div({
-  marginBottom: '20px',
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+  marginBottom: '26px',
   button: {
-    margin: '0 6px',
+    background: `${COLOR_CODE.lightGrey}`,
+    padding: '10px 16px',
+    marginRight: '10px',
+    border: 'none',
+    borderRadius: '6px',
+    cursor: 'pointer',
   },
+});
+
+const Span = styled.div({
+  fontSize: '16px',
+  marginRight: '20px',
 });
--- src/utils/makeChartColors.ts
+++ src/utils/makeChartColors.ts
@@ -8,8 +8,9 @@ export default function makeChartColors(
   selectedRegion: string | null,
 ) {
   return chartDatas.map((data) => {
+    const isAllSelected = selectedRegion === null;
     const isSelectedRegion = data.region === selectedRegion;
 
-    return COLOR[type](isSelectedRegion);
+    return COLOR[type](isAllSelected || isSelectedRegion);
   });
 }
